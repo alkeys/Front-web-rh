5939ca28a50cba10f9d828114afb47b399d7e9b9
alkeys <am18007@ues.edu.sv>
2025-05-08 16:34:18 -0600

    corrigiendo 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import { remix } from '@remix-run/dev/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [remix()]
-});
+  plugins: [react()],
+})
